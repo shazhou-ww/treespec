612bca1fe10eade281671a5a6c617b210e95bc67
xiaomo <xiaomooo@shazhou.work>
2026-08-03 08:32:31 +0000

    refactor: drop shell wrapper, use shebang + symlink for treespec CLI
    - dist/index.js already has #!/usr/bin/env node shebang
    - build script now chmod +x dist/index.js
    - Dockerfile: symlink /usr/local/bin/treespec → /app/dist/index.js
    - package.json: bin points to dist/index.js directly
    - Deleted bin/treespec.js shell wrapper
    
    29/29 self-test passing (196s)

--- bin/treespec.js
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# treespec CLI wrapper
-# Resolve symlink (npm link) to find actual script location
-DIR=$(cd "$(dirname "$(readlink -f "$0")")" && pwd)
-exec node "$DIR/../dist/index.js" "$@"
